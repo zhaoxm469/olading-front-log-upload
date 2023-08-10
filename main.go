45584ff7733e52ecf4100e6d9b82ef50bf045f80
cody <zhaoxm469@163.com>
2023-08-10 18:51:41 +0800

    feat: 增加删除方法

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func writLog(name string) {
-	file, err := os.OpenFile("./log.text", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	file, err := os.OpenFile("./log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		fmt.Println("打开文件出错，err:", err)
 		return
@@ -45,6 +45,20 @@ func main() {
 
 			// 返回一个文本
 			c.String(http.StatusOK, "done")
+		})
+
+		api.GET("/upload/log/clear", func(c *gin.Context) {
+			filePath := "log.txt"
+
+			// 使用 Remove 函数删除文件
+			err := os.Remove(filePath)
+			if err != nil {
+				fmt.Println("Error:", err)
+				c.String(http.StatusOK, "删除失败")
+				return
+			}
+
+			c.String(http.StatusOK, "File deleted successfully.")
 		})
 	}
 
